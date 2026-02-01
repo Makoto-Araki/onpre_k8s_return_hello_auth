54c509216297fe8b2de0234fa0aefd0cab0819d2
Makoto-Araki <mj-araki@ivy.ocn.ne.jp>
2026-02-01 13:12:44 +0900

    feature/implement-bearer-token-01(#5)

--- app/main.py
+++ app/main.py
@@ -1,11 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 # アプリ(ASGI)のインスタンス生成
 app = FastAPI()
 
+# トークン抽出のインスタンス生成
+security = HTTPBearer()
+
+# 固定トークン
+FIXED_TOKEN = "my-secret-token"
+
+# 認証用関数
+def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    if credentials.credentials != FIXED_TOKEN:
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = "Invalid Token",
+        )
+
 # HTTP-GETメソッドで "/" にアクセス時の処理
 @app.get("/")
-def read_root():
+def read_root(_: None = Depends(verify_token)):
     # 辞書を返すと自動的にJSONに変換
     return {"message": "Hello"}
 
