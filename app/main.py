dc304099a7c86d1472d82b81bf7064973ecd9fdb
Makoto-Araki <mj-araki@ivy.ocn.ne.jp>
2026-02-02 20:46:21 +0900

    feature/install-router(#11)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status, APIRouter
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 # アプリ(ASGI)のインスタンス生成
@@ -18,14 +18,31 @@ def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
             detail = "Invalid Token",
         )
 
+# Routerを作成し、dependenciesで一括適用
+router = APIRouter(dependencies=[Depends(verify_token)])
+
+# HTTP-GETメソッドで "/hello1" にアクセス時の処理
+@router.get("/hello1")
+def read_hello1():
+    # 辞書を返すと自動的にJSONに変換
+    return {"message": "Hello1"}
+
+# HTTP-GETメソッドで "/hello2" にアクセス時の処理
+@router.get("/hello2")
+def read_hello2():
+    # 辞書を返すと自動的にJSONに変換
+    return {"message": "Hello2"}
+
+# HTTP-GETメソッドで "/hello3" にアクセス時の処理
+@router.get("/hello3")
+def read_hello3():
+    # 辞書を返すと自動的にJSONに変換
+    return {"message": "Hello3"}
+
+# Routerをアプリに登録
+app.include_router(router)
+
 # HTTP-GETメソッドでヘルスチェック用エンドポイント
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
-
-# HTTP-GETメソッドで "/" にアクセス時の処理
-@app.get("/")
-def read_root(_: None = Depends(verify_token)):
-    # 辞書を返すと自動的にJSONに変換
-    return {"message": "Hello"}
-
